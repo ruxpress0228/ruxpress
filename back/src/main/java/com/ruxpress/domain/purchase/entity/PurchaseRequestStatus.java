7b72da8d5cfec5e4bf5da7edc71940ec6476823f
Kim Yo Han <64533351+rladygks329@users.noreply.github.com>
2026-03-27 15:37:49 +0900

    [feat] 구매요청 구현 REQ-002

---
+++ back/src/main/java/com/ruxpress/domain/purchase/entity/PurchaseRequestStatus.java
@@ -0,0 +1,14 @@
+package com.ruxpress.domain.purchase.entity;
+
+public enum PurchaseRequestStatus {
+    DRAFT,
+    SUBMITTED,
+    REVIEWING,
+    CONFIRMED,
+    PURCHASING,
+    PURCHASED,
+    SHIPPING,
+    DELIVERED,
+    CANCELLED,
+    REFUNDED
+}
